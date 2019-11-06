814cc14f9d87d216f1267f69d20538baefb77fd7
Subha Sarkar <subha7431@gmail.com>
2019-11-06 14:42:18 +0530

    compleated styling

--- src/NewCard.js
+++ src/NewCard.js
@@ -4,7 +4,7 @@ import React, { Fragment } from 'react';
 const Card = ({ name, email, id }) => {
     return (
         <Fragment>
-            <div className="bg-light-green br3 pd3 grow dib ma4 bw2 shadow-5 tc">
+            <div className="bg-washed-red br4 pd3 grow dib ma5 bw2 shadow-5 tc">
                 <img src={`https://robohash.org/${id}?200x200`} alt="photos"/>
                 <h2>{name}</h2>
                 <p>{email}</p> 
